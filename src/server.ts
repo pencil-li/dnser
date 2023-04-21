8d022f869f2ce00ce67d3d85e5ab094dd36403e7
miguelgargallo <mgargallo16@gmail.com>
2023-04-21 22:48:07 +0200

    gh 0.0.5

--- src/server.ts
+++ src/server.ts
@@ -15,8 +15,46 @@ app.get("/api/tld/:tldType", (req, res) => {
   }
 });
 
+const colorize = (text: string, code: number): string => `\u001b[${code}m${text}\u001b[0m`;
+
+const createRoundedBox = (text: string, width: number, height: number): string => {
+  const horizontal = '─'.repeat(width - 2);
+  const vertical = '│';
+
+  const top = `╭${horizontal}╮`;
+  const middle = `${vertical}${' '.repeat(width - 2)}${vertical}`;
+  const bottom = `╰${horizontal}╯`;
+
+  let box = top + '\n';
+  for (let i = 0; i < height - 2; i++) {
+    if (i === Math.floor((height - 2) / 2)) {
+      const textStart = Math.floor((width - text.length) / 2);
+      box += `${vertical}${' '.repeat(textStart)}${text}${' '.repeat(width - text.length - textStart - 2)}${vertical}\n`;
+    } else {
+      box += middle + '\n';
+    }
+  }
+  box += bottom;
+
+  return box;
+};
+
+const clearConsole = () => process.stdout.write('\x1Bc');
+
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-  console.log(`Try http://localhost:${port}/api/tld/hns`);
-  console.log(`Try http://localhost:${port}/api/tld/icann`);
+  clearConsole();
+  console.log(colorize(createRoundedBox("Welcome to the server!", 30, 5), 36));
+  console.log("Loading...");
+
+  setTimeout(() => {
+    clearConsole();
+    console.log(colorize(createRoundedBox("Fun staff loaded and ready!", 30, 5), 33));
+
+    setTimeout(() => {
+      clearConsole();
+      console.log(colorize(`\nServer is listening on port ${port}`, 32));
+      console.log(colorize(`http://localhost:${port}/api/tld/hns`, 34));
+      console.log(colorize(`http://localhost:${port}/api/tld/icann`, 34));
+    }, 10000);
+  }, 10000);
 });
