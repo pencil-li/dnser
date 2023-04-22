6ba43cf32aa40e021d447bf4a88aef8f69764f58
miguelgargallo <mgargallo16@gmail.com>
2023-04-22 21:17:23 +0200

    gh 0.0.5.1

---
+++ src/mockDomainAvailability.ts
@@ -0,0 +1,5 @@
+// mockDomainAvailability.ts
+export const mockDomainAvailability = (domain: string): boolean => {
+    return Math.random() > 0.5; // Returns a random boolean value
+  };
+  
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { getTldInfo } from "./index";
 import { getDnsServers } from "./dns";
+import { mockDomainAvailability } from "./mockDomainAvailability";
 
 const app = express();
 const port = process.env.PORT || 3003;
@@ -8,12 +9,9 @@ const port = process.env.PORT || 3003;
 app.get("/api/tld/:tldType", (req, res) => {
   const tldType = req.params.tldType.toUpperCase();
   const tlds = getTldInfo(tldType as "HNS" | "ICANN");
-
-  // Get the name query parameter
   const name = req.query.name?.toString().toLowerCase();
 
   if (tlds.length > 0) {
-    // Filter the TLD data based on the provided name
     const filteredTlds = name
       ? tlds.filter((tld) => tld.toLowerCase().includes(name))
       : tlds;
@@ -35,12 +33,40 @@ app.get("/api/dns/:dnsType", (req, res) => {
   }
 });
 
+// Updated endpoint for checking domain availability
+app.get("/api/domain/availability", (req, res) => {
+  const domain = req.query.domain?.toString().toLowerCase();
+
+  if (!domain) {
+    res.status(400).json({ error: "Domain parameter is required" });
+    return;
+  }
+
+  // Get the TLDs for both ecosystems
+  const icannTlds = getTldInfo("ICANN");
+  const hnsTlds = getTldInfo("HNS");
+
+  // Extract the TLD from the domain
+  const domainTld = domain.split(".").pop()?.toLowerCase();
+
+  // Check if the domain is an ICANN TLD
+  const isIcannTld = icannTlds.includes(domainTld || "");
+
+  // Check domain availability in both ecosystems
+  const icannAvailable = !isIcannTld && mockDomainAvailability(domain);
+  const hnsAvailable = mockDomainAvailability(domain);
+
+  res.json({
+    ICANN: { domain, available: icannAvailable },
+    HNS: { domain, available: hnsAvailable },
+  });
+});
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
   console.log(`http://localhost:${port}/api/tld/hns`);
   console.log(`http://localhost:${port}/api/tld/icann`);
   console.log(`http://localhost:${port}/api/dns/hns`);
   console.log(`http://localhost:${port}/api/dns/icann`);
-  console.log(`http://localhost:3003/api/tld/icann?name=com`);
-  console.log(`http://localhost:3003/api/tld/hns?name=coin`);
+  console.log(`http://localhost:${port}/api/domain/availability?domain=example`); // Added example domain availability endpoint
 });
